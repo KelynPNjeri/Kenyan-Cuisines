d7339592104228108cfc4d7b174b4188a135f9d7
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-08-23 18:48:16 +0300

    Setup mobile client.

---
+++ app/src/main/java/ke/co/kelynpaul/kenyancuisines/MainActivity.java
@@ -0,0 +1,14 @@
+package ke.co.kelynpaul.kenyancuisines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
